e9a0e5240e8c66a0e23fc035fa3ff95ac93c0beb
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-08-08 16:34:39 -0300

    Add: Animations and improvements on styles

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React, {useRef, useState} from 'react';
 import {
+  Animated,
   Keyboard,
   StyleSheet,
   Text,
@@ -10,17 +11,19 @@ import {
 import api from './src/services/api';
 
 const App = () => {
-
-  const [cep,setCep] = useState(0);
-  const [showInfo,setShowInfo] = useState(false);
-  const [cepInfo,setCepInfo] = useState({})
+  const [cep, setCep] = useState(0);
+  const [showInfo, setShowInfo] = useState(false);
+  const [cepInfo, setCepInfo] = useState({});
   const [requestError, setRequestError] = useState(false);
+  const opacityChanger = new Animated.Value(0);
   const inputField = useRef(null);
+
+  
 
   const requestCep = async () => {
     try {
       const response = await api.get(`${cep}/json`);
-      if(response.status === 200){
+      if (response.status === 200) {
         setRequestError(false);
         setShowInfo(true);
         setCepInfo({...response.data});
@@ -29,41 +32,72 @@ const App = () => {
     } catch (error) {
       setRequestError(true);
     }
-  }
+    
+  };
+
+  Animated.timing(
+    opacityChanger,
+    {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: true
+    }
+  ).start()
 
   const clearAll = () => {
+    Animated.timing(
+      opacityChanger,
+      {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true
+      }
+    ).start()
+    setTimeout(() => {
     setShowInfo(false);
     setCep(0);
     setCepInfo({});
     inputField.current.focus();
-  }
+    }, 500)
+    
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <Text style={styles.title}>Digite seu CEP:</Text>
-        <TextInput style={styles.inputField} keyboardType='numeric' onChangeText={(text) => setCep(text)} value={cep} ref={inputField} placeholder='Ex: 11015000' />
+        <TextInput
+          style={styles.inputField}
+          keyboardType="numeric"
+          onChangeText={text => setCep(text)}
+          value={cep}
+          ref={inputField}
+          placeholder="Ex: 11015000"
+        />
       </View>
       <View style={styles.btnContainer}>
-        <TouchableOpacity style={[styles.btnField, {backgroundColor: 'green'}]} onPress={requestCep}>
+        <TouchableOpacity
+          style={[styles.btnField, {backgroundColor: 'green'}]}
+          onPress={requestCep}>
           <Text style={styles.btnText}>Buscar</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.btnField, {backgroundColor: 'red'}]} onPress={clearAll}>
+        <TouchableOpacity
+          style={[styles.btnField, {backgroundColor: 'red'}]}
+          onPress={clearAll}>
           <Text style={styles.btnText}>Limpar</Text>
         </TouchableOpacity>
       </View>
-      {
-        requestError && <Text style={styles.errorMsg}>Erro ao buscar CEP</Text>
-      }
-      {showInfo && (<View style={styles.cepContainer}>
-        <Text style={styles.cepText}>CEP: {cepInfo.cep}</Text>
-        <Text style={styles.cepText}>Logradouro: {cepInfo.logradouro}</Text>
-        <Text style={styles.cepText}>Complemento: {cepInfo.complemento}</Text>
-        <Text style={styles.cepText}>Bairro: {cepInfo.bairro}</Text>
-        <Text style={styles.cepText}>Localidade: {cepInfo.localidade}</Text>
-        <Text style={styles.cepText}>UF: {cepInfo.uf}</Text>
-      </View>)}
-      
+      {requestError && <Text style={styles.errorMsg}>Erro ao buscar CEP</Text>}
+      {showInfo && (
+        <Animated.View style={[styles.cepContainer, {opacity: opacityChanger}]}>
+          <Text style={styles.cepText}>CEP: {cepInfo.cep}</Text>
+          <Text style={styles.cepText}>Logradouro: {cepInfo.logradouro}</Text>
+          <Text style={styles.cepText}>Complemento: {cepInfo.complemento}</Text>
+          <Text style={styles.cepText}>Bairro: {cepInfo.bairro}</Text>
+          <Text style={styles.cepText}>Localidade: {cepInfo.localidade}</Text>
+          <Text style={styles.cepText}>UF: {cepInfo.uf}</Text>
+        </Animated.View>
+      )}
     </View>
   );
 };
@@ -72,23 +106,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
-  title:{
+  title: {
     fontSize: 30,
     fontWeight: 'bold',
     color: 'black',
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
-  inputContainer:{
-    gap: 10
+  inputContainer: {
+    gap: 10,
   },
-  inputField:{
+  inputField: {
     width: 300,
     borderColor: 'black',
     borderTopWidth: 1,
     borderBottomWidth: 1,
-    fontSize: 18
+    fontSize: 18,
   },
   btnContainer: {
     marginTop: 30,
@@ -96,19 +130,19 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  btnField:{
+  btnField: {
     width: 150,
     height: 45,
     borderRadius: 10,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
-  btnText:{
+  btnText: {
     fontSize: 20,
     color: 'white',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
-  errorMsg:{
+  errorMsg: {
     marginTop: 20,
     color: 'red',
     fontWeight: 'bold',
@@ -116,21 +150,21 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderTopWidth: 1,
   },
-  cepContainer:{
+  cepContainer: {
     marginTop: 20,
     width: 250,
     alignSelf: 'center',
     borderColor: 'black',
-    borderLeftWidth: 4
+    borderLeftWidth: 4,
   },
-  cepText:{
+  cepText: {
     fontSize: 16,
     paddingLeft: 10,
     fontWeight: 'bold',
     color: 'black',
     borderBottomColor: 'black',
-    borderBottomWidth: 0.8
-  }
+    borderBottomWidth: 0.8,
+  },
 });
 
 export default App;
